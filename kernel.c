cbc22dc9023fc284f4506a7d78b2c3fb6afe5f98
mkitane <mehdikitane@gmail.com>
2014-12-12 08:56:13 +0100

    kernel.c simple

--- kernel.c
+++ kernel.c
@@ -1,5 +1,5 @@
 #include "stdlib.h"
-#include "./sched_fixed_priority/sched_fixed.h"
+#include "./sched_simple/sched.h"
 #include "./hardware/hw.h"
 //#include "syscall/syscall.h"
 
@@ -8,11 +8,11 @@ void funcA()
     int cptA = 0;
 
     while (1) {
-	int i,j;
-	for (i=0; i<200000; i++);
-	led_on();
-	for (j=0; j<200000; j++);
-	led_off();
+		int i,j;
+		for (i=0; i<200000; i++);
+		led_on();
+		for (j=0; j<200000; j++);
+		led_off();
     }
 }
 
@@ -20,43 +20,42 @@ void funcB()
 {
 
 int clt =0;
-while (1) {
-	int i,j;
-	for (i=0; i<100000; i++);
-	led_on();
-	for (j=0; j<100000; j++);
-	led_off();
-	clt++;
-
+	while (1) {
+		int i,j;
+		for (i=0; i<100000; i++);
+		led_on();
+		for (j=0; j<100000; j++);
+		led_off();
+		clt++;
 	}
 }
 
 void funcC()
 {
 int clt =0;
-while (clt < 10) {
-	int i,j;
-	for (i=0; i<100000; i++);
-	led_on();
-	for (j=0; j<100000; j++);
-	led_off();
-	clt++;
+	while (clt < 10) {
+		int i,j;
+		for (i=0; i<100000; i++);
+		led_on();
+		for (j=0; j<100000; j++);
+		led_off();
+		clt++;
     }
 }
 
 //------------------------------------------------------------------------
 int kmain ( void )
 {
-  init_hw();
+  	init_hw();
      
     current_process->pt_fct = NULL;
     int stack_size = STACK_SIZE;
-    create_process_fixed(funcB, NULL, stack_size,LOW);
-    create_process_fixed(funcA, NULL, stack_size,MEDIUM);
-    create_process_fixed(funcC, NULL, stack_size,HIGH);
+    create_process(funcB, NULL, stack_size);
+    create_process(funcA, NULL, stack_size);
+    create_process(funcC, NULL, stack_size);
 
     start_sched();
-   // ctx_switch();
+   	// ctx_switch();
     while(1){}
     /* Pas atteignable vues nos 2 fonctions */
     return 0;
